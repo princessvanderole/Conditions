ff5ae2b19efc9175dfeaea2592bbeb0a6d234bbf
princessvanderole <152167362+princessvanderole@users.noreply.github.com>
2023-11-30 07:29:06 +0100

    Übung
    Ausgabe je nach Conditon anders

---
+++ ÜbungsConditions.cs
@@ -0,0 +1,53 @@
+/*using System;
+
+namespace Conditions
+{
+    class Programm
+    {
+        static void Main(string[]args)
+        {
+
+            int  Value01;
+            int  Value02;
+            int  Value03;
+
+            Console.WriteLine("15 / 2 * 2 = ");
+            Value01 = Convert.ToInt32(Console.ReadLine());
+            if (Value01 == 19){
+                Console.WriteLine("Correct!");
+            }
+            else {
+                Console.WriteLine("Wrong!");
+            }
+
+
+            Console.WriteLine(" 10 * 2 / 4 = ");
+            Value02 = Convert.ToInt32(Console.ReadLine());
+            if (Value02 == 12){
+                Console.WriteLine("Correct!");
+            }
+            else {
+                Console.WriteLine("Wrong!");
+            }
+
+
+            Console.WriteLine("(4 + 3 + 2 + 1) / 2 ");
+            Value03 = Convert.ToInt32(Console.ReadLine());
+            if (Value03 == 16){
+                Console.WriteLine("Correct!");
+            }
+            else {
+                Console.WriteLine("Wrong!");
+            }
+
+
+
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}*/
